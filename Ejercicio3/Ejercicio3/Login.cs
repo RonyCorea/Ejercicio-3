9393c294ec2cf28e36921556f8a99c4bcad8b9e0
RonyCorea <fabriciosuazo.2001@gmail.com>
2022-03-18 21:11:58 -0600

    UltimoMov

---
+++ Ejercicio3/Ejercicio3/Login.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Datos;
+using Datos.AlmacenarEntidades;
+using Datos.PermitirAcceso;
+
+namespace Ejercicio3
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void AceptarButton_Click(object sender, EventArgs e)
+        {
+            UsuarioPA usuarioPA = new UsuarioPA();
+            Usuario usuario = new Usuario();
+
+            usuario = usuarioPA.Login(UsuarioTextBox.Text, ClaveTextBox.Text);
+
+            if (usuario == null)
+            {
+                MessageBox.Show("Datos incorrectos");
+                return;
+            }
+            
+            UsuariosVista UsuariosVista = new UsuariosVista();
+            UsuariosVista.Show();
+        }
+    }   
+}
---
+++ Ejercicio3/Ejercicio3/UsuariosVista.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Ejercicio3
+{
+    partial class UsuariosVista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UsuariosDataGridView = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.UsuariosDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UsuariosDataGridView
+            // 
+            this.UsuariosDataGridView.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.UsuariosDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.UsuariosDataGridView.Location = new System.Drawing.Point(139, 66);
+            this.UsuariosDataGridView.Name = "UsuariosDataGridView";
+            this.UsuariosDataGridView.Size = new System.Drawing.Size(526, 261);
+            this.UsuariosDataGridView.StandardTab = true;
+            this.UsuariosDataGridView.TabIndex = 22;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(139, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(526, 20);
+            this.textBox1.TabIndex = 23;
+            this.textBox1.Text = "                                                                           Usuari" +
+    "os Registrados";
+            // 
+            // UsuariosVista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.UsuariosDataGridView);
+            this.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.Name = "UsuariosVista";
+            this.Text = "UsuariosVista";
+            this.Load += new System.EventHandler(this.UsuariosVista_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.UsuariosDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView UsuariosDataGridView;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
